84e43d3d56be36cf105615e187e8f83e6a7679ef
Siyam Sikder <siyam0sikder@gmail.com>
2026-04-19 15:28:30 +0600

    add user and team types or role

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+
+export enum Role {
+    ADMIN = "ADMIN",
+    MANAGER = "MANAGER",
+    UESR = "USER",
+    GUEST = "GUEST",
+}
+
+export interface User {
+    id: string;
+    name : string;
+    email : string;
+    rome : Role;
+    teamId : string;
+    team? : Team;
+    createdAt : Date;
+    UpdatedAt : Date;
+}
+
+export interface Team {
+    id : string;
+    name : string;
+    description?: string;
+    code : string;
+    members :User[];
+    createdAt : Date;
+    UpdatedAt : Date;
+}
